cbcaed39711ee29fc595c2adb03199359d70a553
Kariuki Joseph <kariuki.joseph121@gmail.com>
2023-08-18 13:45:30 +0300

    Move production database credentials to .env

--- src/models/index.js
+++ src/models/index.js
@@ -9,6 +9,14 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
+// use configs in dotenv for production
+if(process.env.NODE_ENV === 'production') {
+  config.database = process.env.DB_NAME;
+  config.username = process.env.DB_USER;
+  config.password = process.env.DB_PASS;
+  config.host = process.env.DB_HOST;
+}
+
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
