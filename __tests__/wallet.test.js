23ab8b0afd0c14cc32798f181e1686999a61613f
Kariuki Joseph <kariuki.joseph121@gmail.com>
2023-08-24 19:48:15 +0300

    Send money tests

--- __tests__/wallet.test.js
+++ __tests__/wallet.test.js
@@ -11,15 +11,15 @@ const userData = generateUsers(20);
 const walletData = generateWallets(20);
 
 beforeAll(async () => {
-    await sequelize.sync({ force: true });
+  await sequelize.sync({ force: true });
 });
 
 beforeEach(async () => {
-    await User.bulkCreate(userData);
-    await Wallet.bulkCreate(walletData);
+  await User.bulkCreate(userData);
+  await Wallet.bulkCreate(walletData);
 });
 afterEach(async () => {
-    await sequelize.sync({ force: true });
+  await sequelize.sync({ force: true });
 });
 
 afterAll(async () => {
@@ -30,9 +30,108 @@ describe("GET /api/v1/wallet/balance", () => {
   it("should get the balance of the user given user phone number", async () => {
     const response = await request
       .get("/api/v1/wallet/balance")
-      .send({ phone: "0114662464" });
+      .query({ phone: "0114662464" });
+
     expect(response.status).toBe(200);
+
     expect(response.body.data).toHaveProperty("balance");
     expect(response.body.data.balance).toBe(walletData[1].balance);
   });
+});
+
+describe("POST /api/v1/wallet/send-money", () => {
+  it("should fail if sender does not have a verified phone number", async () => {
+    const response = await request.post("/api/v1/wallet/send-money").send({
+      senderPhone: "07122222222",
+      receiverPhone: "07122222223",
+      amount: 20,
+    });
+
+    expect(response.status).toBe(404);
+    expect(response.body.success).toBe(false);
+  });
+
+  it("should fail to send if the receiver does not have a verified phone number", async () => {
+    const response = await request.post("/api/v1/wallet/send-money").send({
+      senderPhone: "0114662464",
+      receiverPhone: "07122222223",
+      amount: 20,
+    });
+
+    expect(response.status).toBe(404);
+    expect(response.body.success).toBe(false);
+  });
+
+  it("should send money from one user to another", async () => {
+    const senderPhone = "0114662464";
+    const receiverPhone = "0712345678";
+    const amount = 20;
+
+    let response = await request.get("/api/v1/wallet/balance").query({
+      phone: senderPhone,
+    });
+
+    expect(response.body.success).toBe(true);
+    const originalSenderBal = response.body.data.balance;
+
+    response = await request.post("/api/v1/wallet/send-money").send({
+      senderPhone,
+      receiverPhone,
+      amount: 20,
+    });
+
+    expect(response.status).toBe(200);
+    expect(response.body.success).toBe(true);
+    expect(originalSenderBal - amount).toBe(response.body.data.new_balance);
+  });
+
+  it("on sending, it should deduct the amount from sender and credit the receiver", async () => {
+    const senderPhone = "0114662464";
+    const receiverPhone = "0712345678";
+    const amount = 20;
+
+    let senderBalance = await request.get("/api/v1/wallet/balance").query({
+      phone: senderPhone,
+    });
+
+    const receiverBalance = await request.get("/api/v1/wallet/balance").query({
+      phone: receiverPhone,
+    });
+
+    const senderBal = senderBalance.body.data.balance;
+    const receiverBal = receiverBalance.body.data.balance;
+
+    const response = await request.post("/api/v1/wallet/send-money").send({
+      senderPhone,
+      receiverPhone,
+      amount,
+    });
+
+    expect(senderBal - amount).toBe(response.body.data.new_balance);
+
+    // get new receiver balance
+    const newReceiverBalance = await request
+      .get("/api/v1/wallet/balance")
+      .query({
+        phone: receiverPhone,
+      });
+
+    expect(newReceiverBalance.body.success).toBe(true);
+    expect(receiverBal + amount).toBe(newReceiverBalance.body.data.balance);
+  });
+
+  it("should fail to send money if you do not have enough balance", async () => {
+    const senderPhone = "0712345678"; // has an initial balance of 0
+    const receiverPhone = "0114662464";;
+    const amount = 20;
+
+    const response = await request.post("/api/v1/wallet/send-money").send({
+      senderPhone,
+      receiverPhone,
+      amount
+    });
+    
+    expect(response.status).toBe(400);
+    expect(response.body.success).toBe(false);
+  })
 });
