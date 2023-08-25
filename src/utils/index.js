faddab874491f1c29ef07a924943df02b96ee38d
Kariuki Joseph <kariuki.joseph121@gmail.com>
2023-08-25 06:40:41 +0300

    feat: withdraw cash

--- src/utils/index.js
+++ src/utils/index.js
@@ -6,8 +6,29 @@ function camelCase(str) {
   return str.replace(/_[a-z]/g, (letter) => letter[1].toUpperCase());
 }
 
+// format timestamp to 25 August 2023, 06:30 AM
+function formatTimestamp(timestamp) {
+  const months = [
+    "January","February","March","April","May","June","July","August","September","October","November","December"
+  ];
+
+  const date = new Date(timestamp);
+  const day = date.getDate();
+  const month = months[date.getMonth()];
+  const year = date.getFullYear();
+
+  let hours = date.getHours();
+  const ampm = hours >= 12 ? "PM" : "AM";
+  hours = hours % 12 || 12; // convert to 12-hour format
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+
+  const formattedTimestamp = `${day} ${month} ${year}, ${hours}:${minutes} ${ampm}`;
+  return formattedTimestamp;
+}
+
 
 module.exports = {
     snakeCase,
-    camelCase
+    camelCase,
+    formatTimestamp
 }
