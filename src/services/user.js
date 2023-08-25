a813d537e8d8f865a2399188df1b6bfefe15cc6e
Kariuki Joseph <kariuki.joseph121@gmail.com>
2023-08-25 16:29:50 +0300

    throw error to controller, instead of in catching it in service layer

--- src/services/user.js
+++ src/services/user.js
@@ -1,111 +1,107 @@
+const PhoneNotRegisteredError = require("../errors/PhoneNotRegisteredError");
+const PhoneNotVerifiedError = require("../errors/PhoneNotVerifiedError");
 const { User } = require("../models");
 const user = require("../models/user");
 const { hashPassword } = require("../services/auth");
 
 const findByGoogleId = async (googleId) => {
-  try {
-    const user = await User.findOne({ where: { googleId } });
-    if (!user) return null;
-    return user;
-  } catch (error) {
-    throw error;
-  }
+  const user = await User.findOne({ where: { googleId } });
+  if (!user) return null;
+
+  return user;
 };
 
 const findByEmail = async (email) => {
-  try {
-    const foundUser = await User.findOne({
-      where: {
-        email: email,
-      },
-    });
-    if (!foundUser) return null;
-    return foundUser;
-  } catch (error) {
-    throw error;
-  }
+  const foundUser = await User.findOne({
+    where: {
+      email: email,
+    },
+  });
+  if (!foundUser) return null;
+
+  return foundUser;
 };
 
 const findByPhone = async (phone) => {
-  try {
-    const user = await User.findOne({
-      where: {
-        phone,
-      },
-    });
-    if (!user) return null;
+  const user = await User.findOne({
+    where: {
+      phone,
+    },
+  });
+  if (!user) return null;
 
-    return user;
-  } catch (error) {
-    throw error;
+  return user;
+};
+
+const ensurePhoneRegistered = async (phone) => {
+  const phoneRegistered = await findByPhone(phone);
+  if (!phoneRegistered) {
+    throw new PhoneNotRegisteredError(phone);
   }
+  return true;
+};
+
+const ensurePhoneVerified = async (phone) => {
+  const user = await findByPhone(phone);
+  if (!user) {
+    throw new PhoneNotRegisteredError(phone);
+  }
+  if (!user.isVerified) {
+    throw new PhoneNotVerifiedError(phone);
+  }
+
+  return true;
 };
 
 const saveUser = async (data) => {
   const { password } = data;
   const hashedPassword = hashPassword(password);
 
-  try {
-    const user = await User.create({
-      ...data,
-      password: hashedPassword,
-    });
-    return user;
-  } catch (error) {
-    console.log("Create user error: ", error);
-    throw error;
-  }
+  const user = await User.create({
+    ...data,
+    password: hashedPassword,
+  });
+
+  return user;
 };
 
 const deleteUser = async (email) => {
-  try {
-    const deleteUser = await User.destroy({
-      where: {
-        email,
-      },
-    });
-    if (!deleteUser) return false;
+  const deleteUser = await User.destroy({
+    where: {
+      email,
+    },
+  });
 
-    return true;
-  } catch (error) {
-    console.log("Delete User error: ", error);
-    throw error;
-  }
+  if (!deleteUser) return false;
+
+  return true;
 };
 
 const updatePhoneNumber = async (userId, phoneNumber) => {
-  try {
-    const updatedUser = await User.update(
-      { phone: phoneNumber },
-      { where: { id: userId } }
-    );
+  const updatedUser = await User.update(
+    { phone: phoneNumber },
+    { where: { id: userId } }
+  );
 
-    if (!updatedUser) {
-      throw new Error("Could not update user Phone Number");
-    }
-
-    return updatedUser;
-  } catch (error) {
-    throw error;
+  if (!updatedUser) {
+    throw new Error("Could not update user Phone Number");
   }
+
+  return updatedUser;
 };
 
 const setVerified = async (userId) => {
-  try {
-    const updatedUser = await User.update(
-      { isVerified: true },
-      { where: { id: userId } }
-    );
+  const updatedUser = await User.update(
+    { isVerified: true },
+    { where: { id: userId } }
+  );
 
-    if (!updatedUser) {
-      throw new Error("Could not update user verification status");
-    }
-
-    return updatedUser;
-  } catch (error) {
-    throw error;
+  if (!updatedUser) {
+    throw new Error("Could not update user verification status");
   }
-}
+
+  return updatedUser;
+};
 
 module.exports = {
   findByGoogleId,
@@ -115,4 +111,6 @@ module.exports = {
   deleteUser,
   updatePhoneNumber,
   setVerified,
+  ensurePhoneRegistered,
+  ensurePhoneRegistered
 };
