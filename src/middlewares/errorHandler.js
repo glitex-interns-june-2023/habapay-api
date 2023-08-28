355dbf055854d8749957667fac0eeecf085d05b1
Kariuki Joseph <kariuki.joseph121@gmail.com>
2023-08-28 14:03:52 +0300

    get all transactions

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,4 +1,5 @@
 const errorHandler = (err, req, res, next) => {
+  console.log("error: ", err)
   const statusCode = err.statusCode || 500;
   return res.status(statusCode).json({
     success: false,
