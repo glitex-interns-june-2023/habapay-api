fd1294fe88de1bacb73d2b9ab6046d23b3c2325a
Kariuki Joseph <kariuki.joseph121@gmail.com>
2023-09-06 07:57:37 +0300

    enable model syncing

--- src/app.js
+++ src/app.js
@@ -41,13 +41,13 @@ app.use("/api/v1/analytics", analyticsRouter);
 // error handler
 app.use(errorHandler);
 
-// sequelize
-//   .sync({ alter: true })
-//   .then(() => {
-//     console.log("Models synced successfully");
-//   })
-//   .catch((err) => {
-//     console.log("Error syncing models", err);
-//   });
+sequelize
+  .sync({ alter: true })
+  .then(() => {
+    console.log("Models synced successfully");
+  })
+  .catch((err) => {
+    console.log("Error syncing models", err);
+  });
 
 module.exports = app;
