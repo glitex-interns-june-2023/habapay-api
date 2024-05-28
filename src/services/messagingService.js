b1b58cd58a62018c7a910db8fef73deb9463c9f8
Kariuki Joseph <kariuki.joseph121@gmail.com>
2024-05-28 13:53:30 +0300

    add generating and sending of OTP

--- src/services/messagingService.js
+++ src/services/messagingService.js
@@ -1,87 +1,40 @@
 const axios = require("axios");
 const nodemailer = require("nodemailer");
 const AfricasTalking = require("africastalking");
+const verificationService = require("./verificationService");
 
 const sendOTP = async (phoneNumber) => {
-
-  // format phone number 
-  phoneNumber = `+254${phoneNumber.substring(phoneNumber.length - 9)}`;
+  // format phone number
+  let formattedPhoneNumber = `+254${phoneNumber.substring(phoneNumber.length - 9)}`;
 
   const credentials = {
     apiKey: process.env.AFRICASTALKING_API_KEY, // use your sandbox app API key for development in the test environment
     username: process.env.AFRICASTALKING_USERNAME, // use 'sandbox' for development in the test environment
   };
 
-  console.log("credentials: ", credentials)
-
   const africastalking = AfricasTalking(credentials);
 
   // Initialize a service e.g. SMS
   let sms = africastalking.SMS;
 
-  console.log("phoneNumber: ", phoneNumber)
+  const otp = Math.floor(100000 + Math.random() * 900000);
+
   // Use the service
   let options = {
-    to: phoneNumber,
-    message: "Your message goes here",
+    to: formattedPhoneNumber,
+    message: `Your HabaPay Verification OTP is:  ${otp}`,
   };
 
   // Send message and capture the response or error
   try {
-    const response = await sms.send(options);
-    console.log(response);
-    return response;
+    // const response = await sms.send(options);
+    // save OTP to database
+    await verificationService.saveOtp(phoneNumber, otp);
+    
+    // return response;
+    return true;
   } catch (error) {
     console.log(error);
-    throw error;
-  }
-};
-
-const verifyOTP = async (phoneNumber, otp) => {
-  const accountSid = process.env.TWILIO_ACCOUNT_SID;
-  const authToken = process.env.TWILIO_AUTH_TOKEN;
-  phoneNumber = `+254${phoneNumber.substring(phoneNumber.length - 9)}`;
-
-  try {
-    // verify OTP using twilio verify
-    const URL =
-      "https://verify.twilio.com/v2/Services/VAaad951ebd0d8efdeec90fed1aeccc411/VerificationCheck";
-
-    const data = new URLSearchParams({
-      To: phoneNumber,
-      Code: otp,
-    });
-    const auth = `Basic ${Buffer.from(`${accountSid}: ${authToken}`).toString(
-      "base64"
-    )}`;
-
-    const options = {
-      headers: {
-        Authorization: auth,
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-    };
-
-    try {
-      const response = await axios.post(URL, data, options);
-      const status = response.data.status;
-
-      if (status != "approved") {
-        const error = new Error("Verification failed: Invalid OTP");
-        error.statusCode = 401;
-        throw error;
-      }
-
-      return data;
-    } catch (error) {
-      // axios error handling
-      const statusCode = error.response.status || 500;
-      const message = error.response.data.message || "Verification failed";
-      error = new Error(message);
-      error.statusCode = statusCode;
-      throw error;
-    }
-  } catch (error) {
     throw error;
   }
 };
@@ -163,7 +116,6 @@ const sendEmail = async (mailOptions) => {
 
 module.exports = {
   sendOTP,
-  verifyOTP,
   sendPin,
   sendVerificationEmail,
   sendEmail,
