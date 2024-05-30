ae7dc79d2f081eec6e54ecf0373ae8f03b14a3ed
Kariuki Joseph <kariuki.joseph121@gmail.com>
2024-05-30 16:55:42 +0300

    make start end endDate for statment generation optional

--- src/routes/statements.js
+++ src/routes/statements.js
@@ -4,10 +4,18 @@ const { body } = require("express-validator");
 const { validateInput } = require("../middlewares/inputValidation");
 const { downloadStatement } = require("../controllers/statementController");
 
-router.post("/download", [
-    body("transactionType").notEmpty().withMessage("Transaction type is required"),
-    body("startDate").isDate(),
-    body("endDate").isDate(),
+router.post(
+  "/download",
+  [
+    body("transactionType")
+      .notEmpty()
+      .withMessage("Transaction type is required"),
+    body("startDate").optional().isDate(),
+    body("endDate").optional().isDate(),
     body("email").isEmail().withMessage("Email is required"),
-    validateInput,  
-], downloadStatement);
+    validateInput,
+  ],
+  downloadStatement
+);
+
+module.exports = router;
