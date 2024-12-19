89fe067d3b6920cc511a9d4bb4641f23f1f86f5c
Kariuki Joseph <kariuki.joseph121@gmail.com>
2024-12-19 21:58:56 +0300

    fix: format timestamp with Africa/Nairobi timestamp

--- src/utils/index.js
+++ src/utils/index.js
@@ -8,32 +8,19 @@ function camelCase(str) {
 
 // format timestamp to 25 August 2023, 06:30 AM
 function formatTimestamp(timestamp) {
-  const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true,
+    timeZone: "Africa/Nairobi", // Specify the Nairobi timezone
+  };
 
-  const date = new Date(timestamp);
-  const day = date.getDate();
-  const month = months[date.getMonth()];
-  const year = date.getFullYear();
+  const formatter = new Intl.DateTimeFormat("en-US", options);
+  const formattedTimestamp = formatter.format(new Date(timestamp));
 
-  let hours = date.getHours();
-  const ampm = hours >= 12 ? "PM" : "AM";
-  hours = hours % 12 || 12; // convert to 12-hour format
-  const minutes = String(date.getMinutes()).padStart(2, "0");
-
-  const formattedTimestamp = `${day} ${month} ${year}, ${hours}:${minutes} ${ampm}`;
   return formattedTimestamp;
 }
 
